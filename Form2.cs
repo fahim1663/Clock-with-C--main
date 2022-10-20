aec8c469e2d79ade4a348a0adad6807fc6ea660b
fahim1663 <86849359+fahim1663@users.noreply.github.com>
2022-10-21 02:37:09 +0600

    Add files via upload

---
+++ Form2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Digital_Clock
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string user, pass;
+            user = textBox1.Text;
+            pass = textBox2.Text;
+            if (user == "Monsur" && pass == "191071021")
+            {
+                Form1 f1 = new Form1();
+                f1.Show();
+                Hide();
+            }
+            else
+            {
+                MessageBox.Show("Wrong Password");
+            }
+        }
+    }
+}
